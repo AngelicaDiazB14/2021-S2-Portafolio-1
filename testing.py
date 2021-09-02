2b28b61b28a635f1ed0895bf755b078146f837cf
Cristian Campos Agüero <ccampos04@gmail.com>
2021-09-01 21:53:52 -0600

    Update testing.py

--- testing.py
+++ testing.py
@@ -14,24 +14,37 @@ def test_potenciaRecusivo_1():
 def test_divisionRecusivo_1():
     assert portafolio1.divisionRecusivo(25, 5) == 5
     
+def test_divisionRecusivo_2():
+    assert portafolio1.divisionRecusivo(25, 0) == 'Error: División entre cero'    
+    
 ###########################################################################    
 
 def test_corrimientoAEntero_1():
     assert portafolio1.corrimientoAEntero(133.5) == 1335
+    
+def test_corrimientoAEntero_2():
+    assert portafolio1.corrimientoAEntero(-133.5) == -1335    
     
 ###########################################################################    
 
 def test_contarDigitosFlotante_1():
     assert portafolio1.contarDigitosFlotante(133.578) == 6
     
+def test_contarDigitosFlotante_2():
+    assert portafolio1.contarDigitosFlotante(-133.578) == 6    
+    
 ###########################################################################    
 
 def test_indiceNumero_1():
     assert portafolio1.indiceNumero(1335, 3) == 5
+    
+def test_indiceNumero_2():
+    assert portafolio1.indiceNumero(1335, 8) == 'Error: Indice fuera del rango del número'
     
 ###########################################################################    
 
 def test_cortarNumero_1():
     assert portafolio1.cortarNumero(1335, 1, 2) == 33
 
-    
+def test_cortarNumero_2():
+    assert portafolio1.cortarNumero(1335, 8, 2) == 'Error: Indices fuera del rango del número'  
